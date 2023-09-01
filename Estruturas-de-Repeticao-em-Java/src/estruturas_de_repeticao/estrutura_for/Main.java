bd893e0a6fd96e908f441061743bbda826ddc527
IcaroRaphael <icaro2551@gmail.com>
2023-09-01 11:06:33 -0300

    feat: Estrutura For

---
+++ Estruturas-de-Repeticao-em-Java/src/estruturas_de_repeticao/estrutura_for/Main.java
@@ -0,0 +1,17 @@
+package estruturas_de_repeticao.estrutura_for;
+
+public class Main {
+    public static void main(String[] args) {
+        // Exemplo 1:
+        for(int carneirinhos = 1 ; carneirinhos <=20; carneirinhos ++) {
+            System.out.println(carneirinhos + " - Carneirinho(s)");
+        }
+
+        // Exemplo 2:
+        String alunos[] = { "FELIPE", "JONAS", "JULIA", "MARCOS" };
+
+        for (int x=0; x<alunos.length; x++) {
+            System.out.println("O aluno no indice x=" + x + " é " + alunos[x]);
+        }
+    }
+}
