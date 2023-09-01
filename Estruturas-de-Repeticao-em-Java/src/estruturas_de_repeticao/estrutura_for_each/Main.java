0d0a440ea2dd7299d643df96a6d6100a46904246
IcaroRaphael <icaro2551@gmail.com>
2023-09-01 11:10:12 -0300

    feat: Estrutura For Each

---
+++ Estruturas-de-Repeticao-em-Java/src/estruturas_de_repeticao/estrutura_for_each/Main.java
@@ -0,0 +1,12 @@
+package estruturas_de_repeticao.estrutura_for_each;
+
+public class Main {
+    public static void main(String[] args) {
+        String[] alunos =  {"FELIPE","JONAS","JULIA","MARCOS"};
+
+        //Forma abreviada
+        for(String aluno : alunos) {
+            System.out.println(aluno);
+        }
+    }
+}
